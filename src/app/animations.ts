33e317479b9290e02228d0de6a31fa2ff705a16e
Ralph Wiest <lioncrazed@gmail.com>
2018-10-15 16:27:43 -0400

    upgraded project

---
+++ src/app/animations.ts
@@ -0,0 +1,46 @@
+
+import { trigger, state, transition, style, animate, keyframes, animation, useAnimation } from '@angular/animations';
+
+export let bounceOutLeftAnimation = animation(
+    animate('.5s ease-out', keyframes([
+        style({
+            offset: .2,
+            opacity: 1,
+            transform: 'translateX(20px)'
+        }),
+        style({
+            offset: 1,
+            opacity: 0,
+            transform: 'translateX(-100%)'
+        })
+    ]))
+);
+
+export let slide = trigger('slide', [
+    transition(':enter', [
+        style({ transform: 'translateX(-10px)' }),
+        animate(500)
+    ]),
+    transition(':leave',
+        useAnimation(bounceOutLeftAnimation)
+    )
+]);
+
+export let fadeInAnimation = animation([
+    style({ opacity: 0 }),
+    animate( '{{ duration }} {{ easing }}')
+], {
+    params: {
+        duration: '2s',
+        easing: 'ease-out'
+    }
+});
+
+export let fade = trigger('fade', [
+    transition(':enter',
+        useAnimation(fadeInAnimation)
+    ),
+    transition(':leave', [
+        animate(2000, style({ opacity: 0 }))
+    ])
+]);
---
+++ src/app/todos/todos.component.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+import { fade, slide, bounceOutLeftAnimation, fadeInAnimation } from './../animations';
+import { trigger, state, transition, style, animate, keyframes, useAnimation, query, animateChild, group, stagger } from '@angular/animations';
+
+@Component({
+  selector: 'todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css'],
+  animations: [
+    trigger('todosAnimation', [
+      transition(':enter', [
+        group([
+          query('h1', [
+            style({ transform: 'translateY(-20px)' }),
+            animate(1000)
+          ]),
+          query('@todoAnimation',
+            stagger(200, animateChild()))
+        ])
+      ])
+    ]),
+    trigger('todoAnimation', [
+      transition(':enter', [
+        useAnimation(fadeInAnimation, {
+          params: {
+            duration: '500ms'
+          }
+        })
+      ]),
+      transition(':leave', [
+        style({ backgroundColor: 'red' }),
+        animate(1000),
+        useAnimation(bounceOutLeftAnimation)
+      ])
+    ])
+  ]
+})
+export class TodosComponent {
+  items: any[] = [
+    'Wash the dishes',
+    'Call the accountant',
+    'Apply for a car insurance'];
+
+  addItem(input: HTMLInputElement) {
+    this.items.splice(0, 0, input.value);
+    input.value = '';
+  }
+
+  removeItem(item) {
+    const index = this.items.indexOf(item);
+    this.items.splice(index, 1);
+  }
+
+  animationStarted($event) { }
+
+  animationDone($event) { }
+}
